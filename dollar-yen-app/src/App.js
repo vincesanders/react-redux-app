967bbe2d8079bcaffc5ac687219a0ca4b1209dd6
Vincent Sanders <vincesanders@hotmail.com>
2020-01-23 14:41:03 -0600

    added the Options component.

--- dollar-yen-app/src/App.js
+++ dollar-yen-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import { Options } from './components';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Options />
     </div>
   );
 }
---
+++ dollar-yen-app/src/components/Options.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Pick a Currency</h1>
+            <button>usd/jpy</button>
+        </div>
+    )
+}
