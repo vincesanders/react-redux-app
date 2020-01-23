213134dc121b2b6fc9860d1f9a4d6847d08ada23
Vincent Sanders <vincesanders@hotmail.com>
2020-01-23 14:47:08 -0600

    added basic boilerplate to index.js in actions directory.

---
+++ dollar-yen-app/src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const FETCHING_DATA_START = "FETCHING_DATA_START";
+export const FETCHING_DATA_SUCCESS = "FETCHING_DATA_SUCCESS";
+export const FETCHING_DATA_FAILURE = "FETCHING_DATA_FAILURE";
+
+export const fetchData = () => {
+    return dispatch => {
+        dispatch({ type: FETCHING_DATA_START });
+        axios
+            .get()
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
